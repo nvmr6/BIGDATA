dc2ed850f10cc5075072b6fe0cad407e01860357
nvmr6 <era1995@naver.com>
2020-12-28 20:43:33 +0900

    12-28

--- DBMS/4_FUNCTION.sql
+++ DBMS/4_FUNCTION.sql
@@ -67,3 +67,98 @@ SELECT ENAME, HIREDATE, TRUNC(HIREDATE, 'MONTH') FROM EMP;
 SELECT ENAME, HIREDATE, ROUND(HIREDATE-9, 'MONTH')+24 FROM EMP;
 SELECT ENAME, HIREDATE, SAL, TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE))*SAL FROM EMP;
 SELECT ENAME, HIREDATE, SAL, NVL(COMM,0), TRUNC((SYSDATE-HIREDATE)/365)*(SAL*12+NVL(COMM,0)) FROM EMP;
+
+--(4) 형변환 함수
+SELECT ENAME, TO_CHAR(HIREDATE, 'YYYY-MM-DD, DY') FROM EMP; --날짜 출력 방식
+SELECT TO_CHAR(SYSDATE, 'AM HH24:MI:SS') FROM DUAL;--시간 출력 방식
+SELECT TO_CHAR(2145008000, 'L99,999,999,999') FROM DUAL; --0: 자릿수 채움, 9: 자릿수 안 채움, $:숫자 앞에 붙음, L:지역 통화 단위
+SELECT * FROM EMP 
+    WHERE HIREDATE BETWEEN TO_DATE('19810501', 'YYYYMMDD') AND TO_DATE('19830501', 'YYYYMMDD'); --연도 네자리
+SELECT ROUND(SYSDATE-TO_DATE('20201130', 'YY/MM/DD')) FROM DUAL;
+
+--(5) NULL관련 함수
+SELECT NVL(COMM,0) FROM EMP; --두 매개변수의 형 일치
+SELECT W.ENAME, NVL(M.ENAME, 'CEO') FROM EMP W, EMP M
+    WHERE W.MGR = M.EMPNO(+);
+SELECT ENAME, NVL(TO_CHAR(MGR), 'CEO') FROM EMP; --형일치
+
+--(6) DECODE 함수
+--(데이터, 값1, 결과1, ..., 그외 결과)
+SELECT ENAME, DEPTNO, DECODE(DEPTNO, 10, 'ACCOUNTING', 20, 'RESEARCH', 30, 'SALES', 40, 'OPERATION', 'ETC') "DNAME" FROM EMP;
+SELECT ENAME,DEPTNO, 
+    CASE DEPTNO
+        WHEN 10 THEN 'ACCOUNTING'
+        WHEN 20 THEN 'RESEARCH'
+        WHEN 30 THEN 'SALES'
+        WHEN 40 THEN 'OPERATION'
+        ELSE 'ETC'
+    END "DNAME"
+    FROM EMP;
+SELECT ENAME, SAL, JOB, 
+    DECODE(JOB, 
+    'ANALYST', SAL*1.1, 
+    'SALESMAN', SAL*1.2, 
+    'MANAGER', SAL*1.3, 
+    'CLERK', SAL*1.4, 
+    'PRESIDENT', SAL*5, 
+    SAL) "NEW WAGE" 
+    FROM EMP 
+    ORDER BY "NEW WAGE" DESC;
+
+--(7) 그외 함수
+SELECT EXTRACT (YEAR FROM HIREDATE) FROM EMP; --연도만 출력
+--LEVEL, START WITH(최상위레벨), CONNECT BY PRIOR(레벨간 연결 조건)
+SELECT LEVEL, LPAD(' ', LEVEL*2)||ENAME, MGR FROM EMP
+    START WITH MGR IS NULL
+    CONNECT BY PRIOR EMPNO = MGR;
+    
+--ex)
+-- 1. 현재 날짜를 출력하고 TITLE에 “Current Date”로 출력하는 SELECT 문장을 기술하시오.
+SELECT SYSDATE "CURRENT DATE" FROM DUAL;
+-- 2. EMP 테이블에서 현재 급여에 15%가 증가된 급여를 계산하여,
+-- 사원번호,이름,업무,급여,증가된 급여(New Salary),증가액(Increase)를 출력하는 SELECT 문장
+SELECT EMPNO, ENAME, JOB, SAL, SAL*1.15 "NEW SALARY", SAL*0.15 "INCREASE" FROM EMP; 
+--3. 이름, 입사일, 입사일로부터 6개월 후 돌아오는 월요일 구하여 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, HIREDATE, NEXT_DAY(ADD_MONTHS(HIREDATE, 6), '월') FROM EMP;
+--4. 이름, 입사일, 입사일로부터 현재까지의 개월수, 급여, 입사일부터 현재까지의 받은 급여의 총계를 출력
+SELECT ENAME, HIREDATE, TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE)),SAL, TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE))*SAL FROM EMP;
+--5. 모든 사원의 이름과 급여(15자리로 출력 좌측의 빈 곳은 “*”로 대치)를 출력
+SELECT ENAME, LPAD(SAL, 15, '*' )FROM EMP;
+--6. 모든 사원의 정보를 이름,업무,입사일,입사한 요일을 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, JOB, HIREDATE, TO_CHAR(HIREDATE, 'DY') FROM EMP;
+--7. 이름의 길이가 6자 이상인 사원의 정보를 이름,이름의 글자수,업무를 출력
+SELECT ENAME, LENGTH(ENAME), JOB FROM EMP WHERE LENGTH(ENAME) >= 6 ;
+--8. 모든 사원의 정보를 이름, 업무, 급여, 보너스, 급여+보너스를 출력
+SELECT ENAME, JOB, SAL, NVL(COMM, 0), SAL+NVL(COMM, 0) FROM EMP;
+-- 9.사원 테이블의 사원명에서 2번째 문자부터 3개의 문자를 추출하시오. 
+SELECT SUBSTR(ENAME,2,3) FROM EMP;
+--10. 사원 테이블에서 입사일이 12월인 사원의 사번, 사원명, 입사일을 검색하시오. 
+--  시스템에 따라 DATEFORMAT 다를 수 있으므로 아래의 방법도 알아보자
+SELECT EMPNO, ENAME, HIREDATE FROM EMP WHERE EXTRACT(MONTH FROM HIREDATE) = '12';
+--11. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
+--EMPNO		ENAME		급여
+--7369		       SMITH		*******800
+--7499		       ALLEN		******1600
+--7521		       WARD		******1250
+--……. 
+SELECT EMPNO, LPAD(ENAME, 10 ,' ' ), LPAD(SAL, 10,'*' ) FROM EMP;
+-- 12. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
+-- EMPNO	 ENAME 	입사일
+-- 7369	  SMITH		1980-12-17
+-- ….
+SELECT EMPNO, ENAME, TO_CHAR(HIREDATE, 'YYYY-MM-DD') FROM EMP;
+--13. 사원 테이블에서 부서 번호가 20인 사원의 사번, 이름, 직무, 급여를 출력하시오.
+    --(급여는 앞에 $를 삽입하고3자리마다 ,를 출력한다)
+SELECT EMPNO, ENAME, JOB, TO_CHAR(SAL, '$999,999') FROM EMP;
+-- 14. 사원 테이블에서 급여에 따라 사번, 이름, 급여, 등급을 검색하는 SQL 문장을 작성 하시 오.
+-- 급여가 0~1000 E / 1001~2000 D / 2001~3000 C / 3001~4000 B / 4001~5000 A
+SELECT EMPNO, ENAME, SAL,  
+    CASE TRUNC((SAL-1)/1000) 
+    WHEN 0 THEN 'E' 
+    WHEN 1 THEN 'D'
+    WHEN 2 THEN 'C'
+    WHEN 3 THEN 'B'
+    WHEN 4 THEN 'A'
+    ELSE 'ETC'
+    END "GRADE"
+    FROM EMP;
