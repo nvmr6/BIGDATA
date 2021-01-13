b0ac240b6984d5ef5f921840b868e0801b324fec
nvmr6 <era1995@naver.com>
2021-01-13 21:05:56 +0900

    01-13

---
+++ HTML_CSS/ch 01_html/02.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+name = prompt("what is your name?", "oh");
+document.write(name + " welcome <br>");
+function chk() {
+    if (frm.tel.value.length < 4) {
+        alert("4자리 이상 입력");
+        return false;
+    }
+}
